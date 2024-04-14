d14a9195ade61370083526d8a7aa91c82e4d2f3e
iqrasarfaraz <160213971+iqrasarfaraz@users.noreply.github.com>
2024-04-14 06:50:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//1) computer will generat a randome number
+//2) user input for guessing number
+//3) computer users input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 7 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "input",
+        message: "please Enter your guess number ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you gussed right number");
+}
+else {
+    console.log("you gussed wrong number");
+}
